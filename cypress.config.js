583ff1c4d999ef05d911263f572b72e160b7a998
Vlada Shypalenko <lacuna2505@gmail.com>
2022-11-27 08:55:08 +0200

    Done

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     viewportHeight: 1200,
     viewportWidth: 1900,
-    baseUrl: 'https://localhost:3000',
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/apiTesting.cy.js
+++ cypress/e2e/apiTesting.cy.js
@@ -1,39 +1,191 @@
-import user from "../fixtures/user.json"
-
-Cypress.Commands.add('registerUser', (user) => {
-  cy.request({
-  method: 'POST',
-  url: '/register',
-  body: {
-    email: 'lacuna2@gmail.com',
-    password: 'Playgames2505!',
-  },
-}).then((response) => {
-  cy.request({
-    method: 'POST',
-    status: 200,
-    headers: { Authorization: 'Bearer ' + resp.body.token },
-    body: user,
-  })
-  
-  })
-})
+import user from "../fixtures/user.json";
 
 
+//let userId;
+
+// before(() => {
+//   cy.request({
+//     method: "POST",
+//     url: "/register",
+//     body: {
+//       email: user.user.email,
+//       password: user.user.password,
+//     },
+//     failOnStatusCode: false,
+//   }).then((response) => {
+//     userId = response.body.user.id;
+//   });
+// });
+
+// after(() => {
+//   cy.request({
+//     method: "DELETE",
+//     url: "/users/" + userId,
+//     failOnStatusCode: false,
+//   });
+// });
 
 it(`Get all posts`, () => {
-  cy.request('GET', '/posts').then( response => {
+  cy.request("GET", "/posts").then((response) => {
     expect(response.status).to.be.eq(200);
-    
-  })
-})
 
-// it(`Get pet by id ${pet.id}`, () => {
-//   cy.request('GET', `/pet/${pet.id}`).then( response => {
-//     expect(response.status).to.be.eq(200);
-//     expect(response.body.id).to.be.eq(pet.id);
-//     expect(response.body.name).to.be.eq(pet.name);
-//     expect(response.body.category.id).to.be.eq(pet.category.id);
-//     expect(response.body.category.name).to.be.eql(pet.category.name);
-//   })
-// })
+    expect(response.headers["content-type"]).to.contain("application/json");
+  });
+});
+
+it(`Get 10 posts`, () => {
+  cy.request("GET", "/posts?_start=0&_end=10").then((response) => {
+    expect(response.status).to.be.eq(200);
+
+    expect(response.body).to.have.length(10);
+
+    expect(response.body.at(0).id).to.be.eq(1);
+
+    expect(response.body.at(-1).id).to.be.eq(10);
+  });
+});
+
+it(`Get posts of 55 & 60 ids`, () => {
+  cy.request("GET", "/posts?id=55&id=60").then((response) => {
+    expect(response.status).to.be.eq(200);
+
+    expect(response.body).to.have.length(2);
+
+    expect(response.body.at(0).id).to.be.eq(55);
+
+    expect(response.body.at(1).id).to.be.eq(60);
+  });
+});
+
+it(`Create a post. Verify HTTP response status code.`, () => {
+  cy.request({ method: "PUT", url: "/664/posts/", failOnStatusCode: false }).then((response) => {
+    expect(response.status).to.be.eq(401);
+  });
+});
+
+it(`Create post with adding access token in header. Verify HTTP response status code. Verify post is created.`, () => {
+  cy.request({
+    method: "POST",
+    url: "/login",
+    body: {
+      email: user.user.email,
+      password: user.user.password,
+    },
+    failOnStatusCode: false,
+  }).then((response) => {
+    const token = response.body.accessToken;
+
+    cy.request({
+      method: "POST",
+      url: "/664/posts/",
+      body: {
+        title: "Test",
+        body: "testtesttest",
+      },
+      headers: {
+        Authorization: "Bearer " + token,
+        "Content-Type": "text/plain",
+      },
+    }).then((response) => {
+      expect(response.status).to.be.eq(201);
+      expect(response.statusText).to.be.eq("Created");
+    });
+  });
+});
+
+
+it(`Create post entity and verify that the entity is created. Verify HTTP response status code. Use JSON in body`, () => {
+  cy.request({
+    method: "POST",
+    url: "/posts",
+    body: JSON.stringify({
+      title: "Test",
+      body: "testtesttest",
+    }),
+  }).then((response) => {
+    expect(response.status).to.be.eq(201);
+    expect(response.statusText).to.be.eq("Created");
+  });
+});
+
+it(`Update non-existing entity. Verify HTTP response status code.`, () => {
+  cy.request({
+    method: "PUT",
+    url: "/posts/3000",
+    failOnStatusCode: false,
+  }).then((response) => {
+    expect(response.status).to.be.eq(404);
+  });
+});
+
+it(`Create post entity and update the created entity. Verify HTTP response status code and verify that the entity is updated`, () => {
+  cy.request({
+    method: "POST",
+    url: "/posts",
+    failOnStatusCode: false,
+    body: {
+      title: "Test",
+      body: "testtesttest",
+    },
+  }).then((response) => {
+    expect(response.status).to.be.eq(201);
+    expect(response.statusText).to.be.eq("Created");
+    expect(response.body.title).to.be.eq("Test");
+    expect(response.body.body).to.be.eq("testtesttest");
+
+    const uid = response.body.id;
+    cy.request({
+      method: "PUT",
+      url: "/posts/" + uid,
+      failOnStatusCode: false,
+      body: {
+        title: "Test2",
+        body: "testtesttest2",
+      },
+    }).then((response) => {
+      expect(response.status).to.be.eq(200);
+      expect(response.statusText).to.be.eq("OK");
+      expect(response.body.title).to.be.eq("Test2");
+      expect(response.body.body).to.be.eq("testtesttest2");
+
+      const uid = response.body.id;
+    });
+  });
+});
+
+it(`Delete non-existing post entity. Verify HTTP response status code`, () => {
+  cy.request({
+    method: "DELETE",
+    url: "/posts/3000",
+    failOnStatusCode: false,
+  }).then((response) => {
+    expect(response.status).to.be.eq(404);
+  });
+});
+
+it(`Create post entity, update the created entity, and delete the entity. Verify HTTP response status code and verify that the entity is deleted`, () => {
+  cy.request({
+    method: "POST",
+    url: "/posts",
+    failOnStatusCode: false,
+    body: {
+      title: "Test",
+      body: "testtesttest",
+    },
+  }).then((response) => {
+    expect(response.status).to.be.eq(201);
+    expect(response.statusText).to.be.eq("Created");
+    expect(response.body.title).to.be.eq("Test");
+    expect(response.body.body).to.be.eq("testtesttest");
+
+    const uid = response.body.id;
+    cy.request({
+      method: "DELETE",
+      url: "/posts/" + uid,
+      failOnStatusCode: false,
+    }).then((response) => {
+      expect(response.status).to.be.eq(200);
+      expect(response.statusText).to.be.eq("OK");
+    });
+  });
+});
